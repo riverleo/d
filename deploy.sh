e6749732e449e56a0d086eb0ededf41689b6c786
riverleo <iam.riverleo@gmail.com>
2019-03-18 14:28:08 +0900

    rebuilding site 2019년 3월 18일 월요일 14시 28분 08초 KST

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+git submodule add -b master git@github.com:riverleo/riverleo.github.io.git public
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
